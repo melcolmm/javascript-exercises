c29fdfb22646e0d4034e70242eed8384641c4796
melcolmm <31358679+melcolmm@users.noreply.github.com>
2024-08-06 21:21:22 +0200

    Completed the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let total = 0;
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
 
+    if (Number.isInteger(numOne) == false || Number.isInteger(numTwo) == false) {
+        return "ERROR";
+    }
+
+    if (numOne * 1 != numOne || numTwo * 1 != numTwo) {
+        return "ERROR";
+    }
+    
+    if (numOne < numTwo) {
+        numStart = numOne;
+        numEnd = numTwo;
+    } else {
+        numStart = numTwo;
+        numEnd = numOne;
+    }
+
+    for (let i = numStart; i <= numEnd; i++) {
+        total = total + i;
+    }
+    return total;
 };
 
 // Do not edit below this line
