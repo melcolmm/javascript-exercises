75e592c5c00dbb804f96cdeae59b866f76ab6204
melcolmm <31358679+melcolmm@users.noreply.github.com>
2024-08-06 09:16:12 +0200

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeats) {
+    let finalString = "";
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < repeats; i++) {
+        finalString = finalString + inputString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
