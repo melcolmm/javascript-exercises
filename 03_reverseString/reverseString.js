67691bc59134f1d1ec64a850e619ed31fd21549e
melcolmm <31358679+melcolmm@users.noreply.github.com>
2024-08-06 19:05:02 +0200

    Completed the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    stringLength = text.length;
+    let reverse = ""
+    for (i = stringLength; i >= 0; i--) {
+        reverse = reverse + text.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
