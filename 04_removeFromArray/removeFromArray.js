5ab80aba2a1a2a69ee96d254d79a4540f8ba24d6
melcolmm <31358679+melcolmm@users.noreply.github.com>
2024-08-06 20:34:04 +0200

    Completed the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(test) {
+    let source = arguments[0];
+    
+    let index = "";
+    for (let i = 1; i < arguments.length; i++) {
+        let remove = arguments[i];
+        for (let j = 0; j < source.length; j++) {
+            index = source.indexOf(remove);
+            if (index >= 0) {
+                source.splice(index,1);
+            }
+        }
+    }
+    return source;
 };
 
 // Do not edit below this line
